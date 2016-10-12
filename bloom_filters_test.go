c72bc940c371841775896a7f50f267bfe13ca707
jack-nie <hust.ud.hui@gmail.com>
2016-10-12 16:09:33 +0800

    format code

--- bloom_filters_test.go
+++ bloom_filters_test.go
@@ -3,83 +3,83 @@ package code_kata
 import "testing"
 
 func TestJoaatHash(t *testing.T) {
-  for _, c := range []struct{
-    want uint
-    key string
-  } {
-    {8847632551651054667, "hello"},
-    {1758146220456233818, "word"},
-  } {
-    got := joaatHash(c.key)
+	for _, c := range []struct {
+		want uint
+		key  string
+	}{
+		{8847632551651054667, "hello"},
+		{1758146220456233818, "word"},
+	} {
+		got := joaatHash(c.key)
 
-    if got != c.want {
-      t.Errorf("test faild, expected %d, got %d", c.want, got)
-    }
-  }
+		if got != c.want {
+			t.Errorf("test faild, expected %d, got %d", c.want, got)
+		}
+	}
 }
 
 func TestDjb2Hash(t *testing.T) {
-  for _, c := range []struct{
-    want uint
-    key string
-  } {
-    {210714636441, "hello"},
-    {6385842145, "word"},
-  } {
-    got := djb2Hash(c.key)
+	for _, c := range []struct {
+		want uint
+		key  string
+	}{
+		{210714636441, "hello"},
+		{6385842145, "word"},
+	} {
+		got := djb2Hash(c.key)
 
-    if got != c.want {
-      t.Errorf("test faild, expected %d, got %d", c.want, got)
-    }
-  }
+		if got != c.want {
+			t.Errorf("test faild, expected %d, got %d", c.want, got)
+		}
+	}
 }
 
 func TestSdbmHash(t *testing.T) {
-  for _, c := range []struct{
-    want uint
-    key string
-  } {
-    {27263685106, "hello"},
-    {245562474, "word"},
-  } {
-    got := sdbmHash(c.key)
+	for _, c := range []struct {
+		want uint
+		key  string
+	}{
+		{27263685106, "hello"},
+		{245562474, "word"},
+	} {
+		got := sdbmHash(c.key)
 
-    if got != c.want {
-      t.Errorf("test faild, expected %d, got %d", c.want, got)
-    }
-  }
+		if got != c.want {
+			t.Errorf("test faild, expected %d, got %d", c.want, got)
+		}
+	}
 }
 
 func TestLoseLoseHash(t *testing.T) {
-  for _, c := range []struct{
-    want uint
-    key string
-  } {
-    {532, "hello"},
-    {444, "word"},
-  } {
-    got := loseLoseHash(c.key)
+	for _, c := range []struct {
+		want uint
+		key  string
+	}{
+		{532, "hello"},
+		{444, "word"},
+	} {
+		got := loseLoseHash(c.key)
 
-    if got != c.want {
-      t.Errorf("test faild, expected %d, got %d", c.want, got)
-    }
-  }
+		if got != c.want {
+			t.Errorf("test faild, expected %d, got %d", c.want, got)
+		}
+	}
 }
 
 func TestBloomFilters(t *testing.T) {
-  for _, c := range []struct{
-    want bool
-    file_path string
-    target string
-  } {
-    {true, "tmp/wordlist.txt", "hello"},
-    {true, "tmp/wordlist.txt", "world"},
-    {true, "tmp/wordlist.txt", "say"},
-  } {
-     got := bloomFilters(c.file_path, c.target)
+	for _, c := range []struct {
+		want      bool
+		file_path string
+		target    string
+	}{
+		{true, "tmp/wordlist.txt", "hello"},
+		{true, "tmp/wordlist.txt", "world"},
+		{true, "tmp/wordlist.txt", "say"},
+	} {
+		got := bloomFilters(c.file_path, c.target)
 
-     if got != c.want {
-       t.Errorf("test failed, expected %s, got %s", c.want, got)
-     }
-  }
+		if got != c.want {
+			t.Errorf("test failed, expected %s, got %s", c.want, got)
+		}
+	}
 }
--- conflicting_objectives.go
+++ conflicting_objectives.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-  "bufio"
-  "os"
-  "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 type Set struct {
-  set map[string]bool
+	set map[string]bool
 }
 
 func (set *Set) add(str string) bool {
-  _, found := set.set[str]
-  set.set[str] = true
-  return !found
+	_, found := set.set[str]
+	set.set[str] = true
+	return !found
 }
 
 func checkWords(file_path string) []string {
-  var arr []string
-  var result []string
-  var length int
-  var set Set
+	var arr []string
+	var result []string
+	var length int
+	var set Set
 
-  set.set = make(map[string]bool)
-  dat, _ := os.Open(file_path)
-  scanner := bufio.NewScanner(dat)
+	set.set = make(map[string]bool)
+	dat, _ := os.Open(file_path)
+	scanner := bufio.NewScanner(dat)
 
-  for scanner.Scan() {
-    word := scanner.Text()
-    length = len(word)
-    if length > 6 {
-      continue
-    } else if length < 6 {
-      set.add(word)
-    } else if length == 6{
-      arr = append(arr, word)
-    }
-  }
+	for scanner.Scan() {
+		word := scanner.Text()
+		length = len(word)
+		if length > 6 {
+			continue
+		} else if length < 6 {
+			set.add(word)
+		} else if length == 6 {
+			arr = append(arr, word)
+		}
+	}
 
-  for _, element := range arr {
-    for i := 1; i < 6; i++ {
-      first := element[0:i]
-      last  := element[i:]
-      if !set.add(first) && !set.add(last) {
-        result = append(result, element)
-        break
-      }
-    }
-  }
-  return result
+	for _, element := range arr {
+		for i := 1; i < 6; i++ {
+			first := element[0:i]
+			last := element[i:]
+			if !set.add(first) && !set.add(last) {
+				result = append(result, element)
+				break
+			}
+		}
+	}
+	return result
 }
 
 func main() {
-  s := checkWords("tmp/wordlist.txt")
-  fmt.Println(s)
+	s := checkWords("tmp/wordlist.txt")
+	fmt.Println(s)
 }
