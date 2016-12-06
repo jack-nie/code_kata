9127addbf71124d6ad24e11dd2e89b084b84823d
jack-nie <hust.ud.hui@gmail.com>
2016-12-06 17:48:37 +0800

    add part two

--- aoc2016/day6.go
+++ aoc2016/day6.go
@@ -26,6 +26,17 @@
 //
 // Given the recording in your puzzle input, what is the error-corrected version of the message being sent?
 //
+// --- Part Two ---
+//
+// Of course, that would be the message - if you hadn't agreed to use a modified repetition code instead.
+//
+// In this modified code, the sender instead transmits what looks like random data, but for each character, the character they actually want to send is slightly less likely than the others. Even after signal-jamming noise, you can look at the letter distributions in each column and choose the least common letter to reconstruct the original message.
+//
+// In the above example, the least common character in the first column is a; in the second, d, and so on. Repeating this process for the remaining characters produces the original message, advent.
+//
+// Given the recording in your puzzle input and this new decoding methodology, what is the original message that Santa is trying to send?
+//
+// Your puzzle answer was odqnikqv.
 
 package main
 
@@ -681,3 +692,5 @@ func main() {
 
 	fmt.Println(str)
 }
+
+// part two: in function Less, change from "<" to ">"
