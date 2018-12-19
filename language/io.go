eeaa3a00a9a01d592816964b203d255638dce08a
jack-nie <hust.ud.hui@gmail.com>
2018-12-19 14:39:18 +0800

    io

---
+++ language/io.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("Hello world! \n")
+	if _, err := io.Copy(os.Stdout, r); err != nil {
+		log.Fatal(err)
+	}
+
+	r1 := strings.NewReader("First reader!\n")
+	r2 := strings.NewReader("Second reader!\n")
+	buf := make([]byte, 8)
+	if _, err := io.CopyBuffer(os.Stdout, r1, buf); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := io.CopyBuffer(os.Stdout, r2, buf); err != nil {
+		log.Fatal(err)
+	}
+
+	r3 := strings.NewReader("Second reader!\n")
+	if _, err := io.CopyN(os.Stdout, r3, 5); err != nil {
+		log.Fatal(err)
+	}
+
+	r4, w := io.Pipe()
+	go func() {
+		fmt.Fprint(w, "Some text to be read.\n")
+		w.Close()
+	}()
+
+	buf2 := new(bytes.Buffer)
+	buf2.ReadFrom(r4)
+	fmt.Println(buf2.String())
+}
