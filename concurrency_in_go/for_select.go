7d3a9a02c36404d7804ab638aa567babc6fba40a
jack <hust.ud.hui@gmail.com>
2019-01-28 15:36:32 +0800

    for select in go

---
+++ concurrency_in_go/for_select.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main() {
+	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("work done!")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done.")
+
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+
+		go func() {
+			defer fmt.Println("new reandStream closure exited.\n")
+			defer close(randStream)
+			for {
+				randStream <- rand.Int()
+			}
+		}()
+		return randStream
+	}
+
+
+	randStream := newRandStream()
+	fmt.Println("3 random ints:")
+
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+}
